714e2703a6124032ad87d7f7f4ca5650ab848cb7
Илья Шокин <mallet322@gmail.com>
2021-09-28 13:16:55 +0300

    1. Динамический список на массиве.

---
+++ src/main/java/ru/job4j/collections/arraylist/List.java
@@ -0,0 +1,11 @@
+package ru.job4j.collections.arraylist;
+
+public interface List<T> extends Iterable<T> {
+
+    void add(T value);
+    T set(int index, T newValue);
+    T remove(int index);
+    T get(int index);
+    int size();
+
+}
