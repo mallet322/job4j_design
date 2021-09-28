f2763250872b66932e17c37b72eb7a0b2f46c352
Илья Шокин <mallet322@gmail.com>
2021-09-28 17:37:20 +0300

    5. Очередь на двух стеках

--- src/main/java/ru/job4j/collections/SimpleStack.java
+++ src/main/java/ru/job4j/collections/SimpleStack.java
@@ -4,12 +4,25 @@ public class SimpleStack<T> {
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
 
+    private int size = 0;
+
     public T pop() {
-        return linked.deleteFirst();
+        var result = linked.deleteFirst();
+        size--;
+        return result;
     }
 
     public void push(T value) {
         linked.addFirst(value);
+        size++;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
 }
---
+++ src/test/java/ru/job4j/collections/SimpleQueueTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.collections;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueueTest {
+
+    @Test
+    public void whenPushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        int rsl = queue.poll();
+        Assert.assertThat(rsl, Matchers.is(1));
+    }
+
+    @Test
+    public void when2PushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        int rsl = queue.poll();
+        Assert.assertThat(rsl, Matchers.is(1));
+    }
+
+    @Test
+    public void when2PushPollPushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.poll();
+        queue.push(2);
+        int rsl = queue.poll();
+        Assert.assertThat(rsl, Matchers.is(2));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.poll();
+    }
+    @Test
+    public void whenPushPushPollAndPush() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        queue.poll();
+        queue.push(3);
+        Assert.assertThat(queue.poll(), Matchers.is(2));
+    }
+}
