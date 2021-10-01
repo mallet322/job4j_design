5d5a18ff780b7776ffc541d4dbcb73e621273e4c
Илья Шокин <mallet322@gmail.com>
2021-10-01 12:44:41 +0300

    7. ListIterator

--- src/main/java/ru/job4j/collections/SimpleStack.java
+++ src/main/java/ru/job4j/collections/SimpleStack.java
@@ -2,7 +2,7 @@ package ru.job4j.collections;
 
 public class SimpleStack<T> {
 
-    private ForwardLinked<T> linked = new ForwardLinked<T>();
+    private final ForwardLinked<T> linked = new ForwardLinked<T>();
 
     private int size = 0;
 
@@ -15,10 +15,6 @@ public class SimpleStack<T> {
     public void push(T value) {
         linked.addFirst(value);
         size++;
-    }
-
-    public int size() {
-        return size;
     }
 
     public boolean isEmpty() {
