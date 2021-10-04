a78d754010596304452cdaf8f16372da984430ba
Илья Шокин <mallet322@gmail.com>
2021-10-04 08:22:28 +0300

    1. Создать модель User

---
+++ src/main/java/ru/job4j/collections/map/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.collections.map;
+
+import java.util.Calendar;
+
+public class User {
+
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User() {
+    }
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+}
