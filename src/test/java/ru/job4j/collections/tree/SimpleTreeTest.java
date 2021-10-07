6c8b38001f522dea0b272897e6362f954ef8fce3
Илья Шокин <mallet322@gmail.com>
2021-10-07 14:18:45 +0300

    1. Создать элементарную структуру дерева - fix

--- src/test/java/ru/job4j/collections/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/collections/tree/SimpleTreeTest.java
@@ -14,8 +14,7 @@ public class SimpleTreeTest {
         tree.add(1, 4);
         tree.add(4, 5);
         tree.add(5, 6);
-        Assert.assertThat(tree.findBy(6).isPresent(), Matchers.is(true)
-        );
+        Assert.assertThat(tree.findBy(6).isPresent(), Matchers.is(true));
     }
 
     @Test
@@ -34,6 +33,23 @@ public class SimpleTreeTest {
         tree.add(4, 5);
         tree.add(5, 6);
         Assert.assertFalse(tree.add(2, 6));
+    }
+
+    @Test
+    public void whenTreeIsBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        Assert.assertTrue(tree.isBinary());
+    }
+
+    @Test
+    public void whenTreeIsNotBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 5);
+        Assert.assertFalse(tree.isBinary());
     }
 
 }
