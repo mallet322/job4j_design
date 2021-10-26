42459f62f56e9a997736dfb6fa9d9573b678485d
Илья Шокин <mallet322@gmail.com>
2021-10-26 13:47:36 +0300

    2. Анализ доступности сервера.

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
@@ -22,6 +23,44 @@ public class AnalizyTest {
         File target = folder.newFile("target.txt");
         Analizy.unavailable(source, target.getAbsolutePath());
         String expected = "10:57:01;11:02:02;";
+        StringBuilder actual = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(actual::append);
+        }
+        Assert.assertThat(expected, Matchers.is(actual.toString()));
+    }
+
+    @Test
+    public void whenGetServerDowntimeInTemporaryFileOnOneDiapason() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("400 10:58:01");
+            out.println("200 11:02:02");
+        }
+        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        String expected = "10:58:01;11:02:02;";
+        StringBuilder actual = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(actual::append);
+        }
+        Assert.assertThat(expected, Matchers.is(actual.toString()));
+    }
+
+    @Test
+    public void whenGetServerDowntimeInTemporaryFileOnTwoDiapason() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("400 10:58:01");
+            out.println("200 11:02:02");
+            out.println("200 11:03:02");
+            out.println("500 12:02:02");
+            out.println("400 12:05:02");
+            out.println("300 13:02:02");
+        }
+        Analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        String expected = "10:58:01;11:02:02;12:02:02;13:02:02;";
         StringBuilder actual = new StringBuilder();
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(actual::append);
