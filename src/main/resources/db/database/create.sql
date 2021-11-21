cac82d49562600afeccc68ba1b59ee7a98484912
Илья Шокин <mallet322@gmail.com>
2021-11-21 09:56:05 +0300

    1. Схема прав пользователи и роли. - fix

--- src/main/resources/db/database/create.sql
+++ src/main/resources/db/database/create.sql
@@ -1,45 +1,3 @@
-create table users
-(
-    id      serial primary key,
-    name    varchar(255),
-    role_id integer
-);
-
-create table item
-(
-    id          serial primary key,
-    item        varchar(255),
-    user_id     integer,
-    state_id    integer,
-    category_id integer
-);
-
-create table category
-(
-    id       serial primary key,
-    category varchar(255)
-);
-
-create table state
-(
-    id    serial primary key,
-    state varchar(255)
-);
-
-create table comments
-(
-    id      serial primary key,
-    comment varchar(255),
-    item_id integer
-);
-
-create table attachs
-(
-    id      serial primary key,
-    attach  varchar(255),
-    item_id integer
-);
-
 create table role
 (
     id   serial primary key,
@@ -56,14 +14,71 @@ create table roles_rules
 (
     id      serial primary key,
     role_id integer,
-    rule_id integer
+    rule_id integer,
+    constraint fk_roles_rules_role
+        foreign key (role_id)
+            references role (id),
+    constraint fk_rule
+        foreign key (rule_id)
+            references rule (id)
 );
 
-alter table item add foreign key (user_id) references users (id);
-alter table item add foreign key (category_id) references category (id);
-alter table item add foreign key (state_id) references state (id);
-alter table comments add foreign key (item_id) references item (id);
-alter table attachs add foreign key (item_id) references item (id);
-alter table users add foreign key (role_id) references role (id);
-alter table roles_rules add foreign key (role_id) references role (id);
-alter table roles_rules add foreign key (rule_id) references rule (id);
+create table users
+(
+    id      serial primary key,
+    name    varchar(255),
+    role_id integer,
+    constraint fk_users_role
+        foreign key (role_id)
+            references role (id)
+);
+
+create table category
+(
+    id       serial primary key,
+    category varchar(255)
+);
+
+create table state
+(
+    id    serial primary key,
+    state varchar(255)
+);
+
+create table item
+(
+    id          serial primary key,
+    item        varchar(255),
+    user_id     integer,
+    category_id integer,
+    state_id    integer,
+    constraint fk_category
+        foreign key (category_id)
+            references category (id),
+    constraint fk_state
+        foreign key (state_id)
+            references state (id),
+    constraint fk_users
+        foreign key (user_id)
+            references users (id)
+);
+
+create table comments
+(
+    id      serial primary key,
+    comment varchar(255),
+    item_id integer,
+    constraint fk_comments_items
+        foreign key (item_id)
+            references item (id)
+);
+
+create table attachs
+(
+    id      serial primary key,
+    attach  varchar(255),
+    item_id integer,
+    constraint fk_attachs_items
+        foreign key (item_id)
+            references item (id)
+);
