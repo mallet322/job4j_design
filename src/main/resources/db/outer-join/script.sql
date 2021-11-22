ffcdce87dcea7a9e496d6b54b9c9e240c21ebd9d
Илья Шокин <mallet322@gmail.com>
2021-11-22 13:09:23 +0300

    0. Join [#6862 #231179] - fix

--- src/main/resources/db/outer-join/script.sql
+++ src/main/resources/db/outer-join/script.sql
@@ -7,4 +7,9 @@ select e.* from emploers e left join departments d on e.department_id = d.id whe
 select d.*, e.* from departments d left join emploers e on d.id = e.department_id order by d.id;
 select d.*, e.* from emploers e right join departments d on e.department_id = d.id order by d.id;
 --
-select t1.name as name_1, t2.name as name_2, (t1.gender || ' + ' || t2.gender) as gender_pair from teens t1 cross join teens t2;
+select t1.name as name_1,
+       t2.name as name_2,
+       (t1.gender || ' + ' || t2.gender) as gender_pair
+  from teens t1
+ cross join teens t2
+where t1.gender != t2.gender;
