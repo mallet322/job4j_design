368e3cc64a711d25b9f7efc57750135d0a8541f5
Илья Шокин <mallet322@gmail.com>
2021-11-22 08:49:20 +0300

    2. Группировка и агрегатные функции [#373618 #231091] - fix

--- src/main/resources/db/groupping-and-aggreagate-functions/script.sql
+++ src/main/resources/db/groupping-and-aggreagate-functions/script.sql
@@ -15,11 +15,9 @@ select p.name,
 select p.name,
        count(d.name) as device_count,
        round(avg(d.price)::numeric, 1) as avg_price
-from devices_people dp
-         join people p
-              on dp.people_id = p.id
-         join devices d
-              on dp.device_id = d.id
-group by p.name
+  from devices_people dp
+  join people p on dp.people_id = p.id
+  join devices d on dp.device_id = d.id
+ group by p.name
 having avg(d.price) > 5000
-order by avg_price asc;
+ order by avg_price asc;
