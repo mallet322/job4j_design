995bd3b908f4fad964b48707f13e22fdf1a8f84c
Илья Шокин <mallet322@gmail.com>
2021-11-30 17:41:23 +0300

    2. SQL Select by company [#2760]

---
+++ src/main/resources/db/exam/init.sql
@@ -0,0 +1,20 @@
+insert into company(id, name) values(1, 'company1');
+insert into company(id, name) values(2, 'company2');
+insert into company(id, name) values(3, 'company3');
+insert into company(id, name) values(4, 'company4');
+insert into company(id, name) values(5, 'company5');
+
+insert into person (id, name, company_id) values(1, 'person1', 1);
+insert into person (id, name, company_id) values(2, 'person2', 1);
+insert into person (id, name, company_id) values(3, 'person3', 1);
+insert into person (id, name, company_id) values(4, 'person4', 2);
+insert into person (id, name, company_id) values(5, 'person5', 2);
+insert into person (id, name, company_id) values(6, 'person6', 3);
+insert into person (id, name, company_id) values(7, 'person7', 4);
+insert into person (id, name, company_id) values(8, 'person8', 3);
+insert into person (id, name, company_id) values(9, 'person9', 5);
+insert into person (id, name, company_id) values(10, 'person10', 2);
+insert into person (id, name, company_id) values(11, 'person11', 2);
+insert into person (id, name) values(12, 'person12');
+insert into person (id, name) values(13, 'person13');
+insert into person (id, name) values(14, 'person14');
