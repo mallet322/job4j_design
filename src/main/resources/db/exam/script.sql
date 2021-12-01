ef13ba989dbfbe46a95eb02a77e9fde5710aaa69
Илья Шокин <mallet322@gmail.com>
2021-12-01 08:56:49 +0300

    2. SQL Select by company [#2760 #234434] - fix

--- src/main/resources/db/exam/script.sql
+++ src/main/resources/db/exam/script.sql
@@ -1,19 +1,11 @@
 select p.name as person_name,
        c.name as company_name
-from person p join company c on p.company_id = c.id
-where p.company_id != 5
-order by p.id;
+  from person p inner join company c on p.company_id = c.id
+ where p.company_id != 5
+ order by p.id;
 ------------------------------------
-with total_persons as
-         (select p.company_id, count(p.company_id) as total
-          from person p
-                   join company c
-                        on c.id = p.company_id
-          group by p.company_id)
-
-select t.company_name, t.total
-from (select row_number() over(order by ts.total desc) as row_num,
-             c.name as company_name,
-             ts.total
-      from total_persons ts join company c on c.id = ts.company_id) t
-where t.row_num = 1;
+select c.name as company_name, count(p.name) total_persons
+  from company c inner join person p on c.id = p.company_id
+ group by c.name
+ order by total_persons desc
+ limit 1;
