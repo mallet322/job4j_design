8418bf41b10afd1bc563920a892a3d33ce70dae0
Elias <mallet322@gmail.com>
2022-01-19 10:12:38 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851] - fix

--- src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportXMLTest.java
@@ -3,8 +3,10 @@ package ru.job4j.ood.srp.report;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.ZoneOffset;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class ReportXMLTest {
 
@@ -12,7 +14,8 @@ public class ReportXMLTest {
     public void generate() {
         MemStore store = new MemStore();
         Calendar now = new GregorianCalendar(2022, Calendar.JANUARY, 18);
-        Employee worker = new Employee("Ivan", null, null, 100);
+        now.setTimeZone(TimeZone.getTimeZone(ZoneOffset.of("+3")));
+        Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
         Report report = new ReportXML(store);
         String ln = "\n";
@@ -22,6 +25,10 @@ public class ReportXMLTest {
                 + "<employees>"
                 + ln
                 + "    <employees>"
+                + ln
+                + "        <fired>2022-01-18T00:00:00+03:00</fired>"
+                + ln
+                + "        <hired>2022-01-18T00:00:00+03:00</hired>"
                 + ln
                 + "        <name>Ivan</name>"
                 + ln
